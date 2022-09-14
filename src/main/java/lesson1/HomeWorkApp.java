56431ebfa07e8c1c3595efcecacc497ebaadf60f
valenkuzenko <valentyna@taxer.ua>
2022-09-14 11:26:03 +0300

    deletion

--- src/main/java/lesson1/HomeWorkApp.java
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -2,7 +2,7 @@ package lesson1;
 
 public class HomeWorkApp {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
         printThreeWords();
         checkSumSign(1,-2);
         printColor(-101);
