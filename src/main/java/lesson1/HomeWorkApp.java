3ec18ab9e1f41c91acb77bba23c06df2b0e03a43
valenkuzenko <valentyna@taxer.ua>
2022-09-14 12:41:24 +0300

    deleted unneeded condition

--- src/main/java/lesson1/HomeWorkApp.java
+++ src/main/java/lesson1/HomeWorkApp.java
@@ -26,7 +26,7 @@ public class HomeWorkApp {
     public static void printColor(int value){
         if (value <= 0) {
             System.out.println("Красный");
-        } else if (value > 0 && value <= 100){
+        } else if (value <= 100){
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
